3ca18482524c7d27325eea3f0154a885ba7a71a5
Vladislav <vsoilol1701@gmail.com>
2023-07-05 14:03:59 +0300

    add student entity and write migration

--- Project.DataAccessLayer/DataAccess/ApplicationDbContext.cs
+++ Project.DataAccessLayer/DataAccess/ApplicationDbContext.cs
@@ -11,8 +11,34 @@ internal class ApplicationDbContext : DbContext, IDbContext
 
     public DbSet<VersionInfo> VersionInfos { get; set; } = null!;
 
+    public DbSet<Student> Students { get; set; } = null!;
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+        Database.EnsureCreated();
+    }
+
+    public Task<int> SaveChangesAuditableEntitiesAsync(Guid userId, CancellationToken cancellationToken = default)
+    {
+        var now = DateTime.Now;
+
+        ChangeTracker.Entries<AuditableEntity>().ToList()
+            .ForEach(x =>
+            {
+                switch (x.State)
+                {
+                    case EntityState.Added:
+                        x.Entity.CreatedById = userId;
+                        x.Entity.CreatedAt = now;
+                        break;
+                    case EntityState.Modified:
+                        x.Entity.ModifiedById = userId;
+                        x.Entity.ModifiedAt = now;
+                        break;
+                }
+            });
+
+        return base.SaveChangesAsync(cancellationToken);
     }
 }
---
+++ Project.Domain/Entities/AuditableEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.Domain.Entities;
+
+public abstract class AuditableEntity : Entity
+{
+    public DateTime CreatedAt { get; set; }
+    
+    public Guid CreatedById { get; set; }
+
+    [ForeignKey(nameof(CreatedById))] 
+    public User CreatedByUser { get; set; } = null!;
+
+    public DateTime? ModifiedAt { get; set; }
+
+    public Guid? ModifiedById { get; set; }
+
+    [ForeignKey(nameof(ModifiedById))]
+    public User? ModifiedByUser { get; set; }
+}
